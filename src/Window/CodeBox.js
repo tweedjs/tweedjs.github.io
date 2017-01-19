43196b69ca9e726985bddba810454db81edb6a33
Emil Persson <emil.persson@creuna.se>
2017-01-19 17:39:48 +0100

    Attempt to fix most fiddles

--- src/Window/CodeBox.js
+++ src/Window/CodeBox.js
@@ -46,6 +46,7 @@ export default class CodeBox {
     if (!/^import render from 'tweed\/render\/dom'/.test(code) && /^class (\w+)/.test(code)) {
       const [, mainClass] = /^class (\w+)/.exec(code)
       code = [
+        ...(/} from 'tweed'/.test(code) ? [] : ["import { mutating, Node } from 'tweed'"]),
         "import render from 'tweed/render/dom'",
         code,
         `render(new ${mainClass}(), document.querySelector('#app'))`
